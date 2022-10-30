6b0c879d285850c267d8c0fc986f63c92c504ea1
nauxscript <nauxscript@gmail.com>
2022-10-30 16:54:29 +0800

    chore: restart learning

--- README.md
+++
@@ -1,2 +0,0 @@
-# naux-vue
-## A mini VUE version to explore HOW VUE3 WORK
--- src/shared/index.ts
+++
@@ -1,5 +0,0 @@
-export const extend = Object.assign
-
-export const isObject = val => val !== null && typeof val === 'object'
-
-export const hasChanged = (val, newVale) => (!Object.is(val, newVale))
