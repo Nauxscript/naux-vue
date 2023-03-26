21cb81d75d62f8fa8a9730619dc1b8d3e9a1844a
nauxscript mac-mini <nauxscript@gmail.com>
2023-03-26 10:52:19 +0800

    feat: setup rollup

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import typescript from '@rollup/plugin-typescript'
+
+export default {
+  input: "./packages/vue/src/index.ts",
+  output: [
+    {
+      file: './packages/vue/dist/naux-vue.cjs',
+      format: 'cjs',
+    }, {
+      file: './packages/vue/dist/naux-vue.mjs',
+      format: 'mjs',
+    }
+  ],
+  plugins:[typescript()]
+}
