ea3a7111a11049bfba068f4d25bbedf26e005a85
nauxscript <nauxscript@gmail.com>
2023-04-11 14:16:56 +0800

    chore: update example of component props

--- packages/vue/examples/component-props/App.js
+++ packages/vue/examples/component-props/App.js
@@ -3,10 +3,9 @@ import { h } from '../../dist/naux-vue.esm-bundler.js'
 export const App = {
   render() {
     return h('div', {}, [
-      h('h2', { class: 'red' }, 'component-props example'),
-      h('p', { class: 'red' }, 'support binding events or setting props on a component'),
+      h('h2', { class: 'red' }, `${this.title} example`),
+      h('p', { class: 'blue' }, 'support binding events or setting props on a component'),
       h('p', {
-        class: 'blue',
         onClick: () => {
           alert('click')
         },
@@ -14,12 +13,12 @@ export const App = {
           // eslint-disable-next-line no-console
           console.log('mouseover')
         },
-      }, `click me plz, count:${this.count}`),
+      }, 'click me plz, or see console when mouse over me'),
     ])
   },
   setup() {
     return {
-      count: 1,
+      title: 'component-props',
     }
   },
 }
