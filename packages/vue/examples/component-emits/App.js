494b83816181db70c8fc462ba248982e39e8fc1f
nauxscript <nauxscript@gmail.com>
2023-04-11 14:24:34 +0800

    chore: add example of component emit

---
+++ packages/vue/examples/component-emits/App.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-console */
+import { h } from '../../dist/naux-vue.esm-bundler.js'
+import { Foo } from './Foo.js'
+
+window.self = null
+
+export const App = {
+  render() {
+    window.self = this
+    const FooCom = h(Foo, {
+      count: 1,
+      onAdd(a, b) {
+        console.log('parent onAdd', a, b)
+      },
+      onFooAdd(msg) {
+        console.log(`parent onFooAdd: ${msg}`)
+      },
+    })
+
+    return h('div', {}, [
+      h('h2', { class: 'red' }, `${this.title} example`),
+      h('p', { class: 'blue' }, 'support emit and passing props from parent to child, and forbid to change the prop'),
+      h('p', {
+        onClick: () => {
+          console.log('click')
+        },
+        onMouseover() {
+          console.log('mouseover')
+        },
+      }, `surprise ${this.msg}`),
+      FooCom,
+    ])
+  },
+  setup() {
+    return {
+      title: 'compoent-emits',
+    }
+  },
+}
---
+++ packages/vue/examples/component-emits/Foo.js
@@ -0,0 +1,28 @@
+import { h } from '../../dist/naux-vue.esm-bundler.js'
+
+/* eslint-disable no-console */
+export const Foo = {
+  setup(props, { emit }) {
+    // props = { count }
+    // NB: count is unchangable
+    props.count++
+    console.log(props)
+    const handleBtnClick = () => {
+      console.log('button is clicked')
+      emit('add', 1, 2)
+      emit('foo-add', 'foo add param')
+    }
+    return {
+      handleBtnClick,
+    }
+  },
+  render() {
+    const button = h('button', {
+      onClick: this.handleBtnClick,
+    }, 'emit button')
+    return h('div', {}, [
+      h('div', {}, `the value of count passing from parent is: ${this.count}`),
+      button,
+    ])
+  },
+}
