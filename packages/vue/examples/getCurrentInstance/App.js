3ac656fc92e35e3a71dcb78808e77ebf1c2883b1
nauxscript <nauxscript@gmail.com>
2023-04-11 14:42:25 +0800

    chore: update example
    ddd

--- packages/vue/examples/getCurrentInstance/App.js
+++ packages/vue/examples/getCurrentInstance/App.js
@@ -1,10 +1,13 @@
-import { h } from '../../dist/naux-vue.esm-bundler.js'
+import { getCurrentInstance, h } from '../../dist/naux-vue.esm-bundler.js'
+import { Foo } from './Foo.js'
 
 export const App = {
+  name: 'App',
   render() {
     return h('div', {}, [
       h('h2', { class: 'red' }, `${this.title} example`),
       h('p', { class: 'blue' }, 'implement getCurrentInstance API, can only use in setup'),
+      h(Foo, {}),
     ])
   },
   setup() {
--- packages/vue/examples/getCurrentInstance/Foo.js
+++ packages/vue/examples/getCurrentInstance/Foo.js
@@ -1,8 +1,10 @@
-import { h } from '../../dist/naux-vue.esm-bundler.js'
+import { getCurrentInstance, h } from '../../dist/naux-vue.esm-bundler.js'
 
 export const Foo = {
+  name: 'Foo',
   setup() {
     const instance = getCurrentInstance()
+    // eslint-disable-next-line no-console
     console.log('Foo instance:', instance)
   },
   render() {
