9fa64d9ca351e48f66edd610bfde4ac3cf5e6ba2
nauxscript <nauxscript@gmail.com>
2023-04-12 15:21:05 +0800

    chore: update example

--- packages/vue/examples/provide-inject/App.js
+++ packages/vue/examples/provide-inject/App.js
@@ -6,16 +6,19 @@ const Daughter = {
   render() {
     return h('div', {}, [
       h('h3', {}, this.title),
-      h('Daughter Content:', {}, `data from App:${this.appData.msg}`),
-      h('Daughter Content:', {}, `data from Mother:${this.appData.msg}`),
+      h('p', {}, `Daughter Content: data from key【App2】set by App component: ${this.appData2.msg}`),
+      h('p', {}, `Daughter Content: data from key【App】set by Mother component: ${this.appData.msg}`),
+      h('p', {}, `Daughter Content: data from key【Symbol('Mother')】set by Mother compoent: ${this.motherData.msg}`),
     ])
   },
   setup() {
     const appData = inject('App')
+    const appData2 = inject('App2')
     const motherData = inject(motherKey)
     return {
       title: 'this is Daughter component',
       appData,
+      appData2,
       motherData,
     }
   },
@@ -26,15 +29,18 @@ const Mother = {
   render() {
     return h('div', {}, [
       h('h3', {}, this.title),
-      h('Mother Content:', {}, `data from App:${this.appData.msg}`),
+      h('p', {}, `Mother Content: data from key【App】set by App component:${this.appData.msg}`),
       h(Daughter),
     ])
   },
   setup() {
-    const appData = inject('App')
-    provide(motherKey, {
-      msg: 'data from App',
+    provide('App', {
+      msg: '【Mother data but use "app" key that the same key as App component】',
     })
+    provide(motherKey, {
+      msg: '【Mother data】',
+    })
+    const appData = inject('App')
     return {
       title: 'this is Mother component',
       appData,
@@ -47,13 +53,16 @@ export const App = {
   render() {
     return h('div', {}, [
       h('h2', { class: 'red' }, `${this.title} example`),
-      h('p', { class: 'blue' }, 'implement getCurrentInstance API, can only use in setup'),
+      h('p', { class: 'blue' }, 'implement provide & inject API, can only use in setup'),
       h(Mother),
     ])
   },
   setup() {
     provide('App', {
-      msg: 'data from App',
+      msg: '【App data】',
+    })
+    provide('App2', {
+      msg: '【another App data】',
     })
     return {
       title: 'provide & inject API',
