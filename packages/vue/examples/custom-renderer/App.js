1256ac9d73b09de294cc8dcfbacb6d2cfdddbe19
nauxscript <nauxscript@gmail.com>
2023-04-16 23:06:05 +0800

    chore: update example

--- packages/vue/examples/custom-renderer/App.js
+++ packages/vue/examples/custom-renderer/App.js
@@ -2,14 +2,23 @@ import { h } from '../../dist/naux-vue.esm-bundler.js'
 
 export const App = {
   render() {
-    return h('div', {}, [
-      h('h2', { class: 'red' }, `${this.title} example`),
-      h('p', { class: 'blue' }, 'support custom renderer, use pixiJs to render vnode in canvas in this example'),
-    ])
+    // return h('Container', {}, [
+    //   h('Text', {
+    //     x: 50,
+    //     y: 220,
+    //   }, `${this.title} example`),
+    //   h('Text', {}, 'support custom renderer, use pixiJs to render vnode in canvas in this example'),
+    // ])
+    return h('Rect', {
+      x: this.x,
+      y: this.y,
+    }, [])
   },
   setup() {
     return {
       title: 'custom-renderer',
+      x: 100,
+      y: 100,
     }
   },
 }
--- packages/vue/examples/custom-renderer/main.js
+++ packages/vue/examples/custom-renderer/main.js
@@ -1,19 +1,41 @@
-// import { createApp, createRenderer } from '../../dist/naux-vue.esm-bundler.js'
-// import { App } from './App.js'
+/* eslint-disable no-console */
+import { createRenderer } from '../../dist/naux-vue.esm-bundler.js'
+import { App } from './App.js'
 
-// const appContainer = new window.PIXI.Application({ width: 640, height: 360 })
+const appContainer = new window.PIXI.Application({ width: 640, height: 640 })
+document.body.appendChild(appContainer.view)
+const renderer = createRenderer({
+  createElement(type) {
+    let ele
+    switch (type) {
+      case 'Rect':
+        ele = new window.PIXI.Graphics()
+        ele.beginFill(0xFF0000)
+        ele.drawRect(0, 0, 100, 100)
+        ele.endFill()
+        break
+      // case 'Container':
+      //   ele = new window.PIXI.Container()
+      //   break
+      default:
+        break
+    }
+    if (ele)
+      return ele
+  },
+  patchProp(el, key, val) {
+    el[key] = val
+  },
+  insert(el, parent) {
+    console.log(el)
+    console.log(parent)
+    // if (parent.stage)
+    // parent.stage.addChild(el)
+    // else
+    parent.addChild(el)
+  },
+})
 
-// const renderer = createRenderer({
-//   createElement() {
-
-//   },
-//   patchProp() {
-
-//   },
-//   insert() {
-
-//   },
-// })
-
+renderer.createApp(App).mount(appContainer.stage)
 // const appContainer = document.getElementById('app')
 // createApp(App).mount(appContainer)
