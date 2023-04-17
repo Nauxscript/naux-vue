e1d5e2e5126c6fc20152bb619a8d10ca2013eb7a
nauxscript <nauxscript@gmail.com>
2023-04-17 16:44:24 +0800

    chore: update example

--- packages/vue/examples/hello-world/App.js
+++ packages/vue/examples/hello-world/App.js
@@ -1,6 +1,6 @@
 import { h } from '../dist/naux-vue.esm-bundler.js'
 
-const examples = ['component-props', 'component-emits', 'component-slots', 'getCurrentInstance', 'provide-inject', 'custom-renderer']
+const examples = ['component-props', 'component-emits', 'component-slots', 'getCurrentInstance', 'provide-inject', 'custom-renderer', 'update-process']
 
 export const App = {
   render() {
