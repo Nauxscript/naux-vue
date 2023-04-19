999aa56b89d1d5874049d8e03b9221b2f5ba8be2
nauxscript <nauxscript@gmail.com>
2023-04-19 12:26:58 +0800

    chore: update example

--- packages/vue/examples/update-children/App.js
+++ packages/vue/examples/update-children/App.js
@@ -9,13 +9,13 @@ export const App = {
     return h('div', {}, [
       h('h2', { class: 'red' }, `${this.title} example`),
       h('p', { class: 'blue' }, 'there are following four cases when patching a component\'s children '),
-      h('h3', {}, 'change array children to text children'),
+      h('h3', {}, 'change array children to text children; change window.att.value to false in devtool to check the children rerender'),
       h(Array2Text),
-      h('h3', {}, 'change array children to another array children'),
+      h('h3', {}, 'change array children to another array children; change window.ata.value to false in devtool to check the children rerender'),
       h(Array2Array),
-      h('h3', {}, 'change text children to array children'),
+      h('h3', {}, 'change text children to array children; change window.tta.value to false in devtool to check the children rerender'),
       h(Text2Array),
-      h('h3', {}, 'change text children to another text children'),
+      h('h3', {}, 'change text children to another text children; change window.ttt.value to false in devtool to check the children rerender'),
       h(Text2Text),
     ])
   },
