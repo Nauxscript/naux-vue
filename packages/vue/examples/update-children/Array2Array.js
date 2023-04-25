225c50f7fa04eb3206c5c2d7d2cc558078af557f
nauxscript <nauxscript@gmail.com>
2023-04-25 10:22:42 +0800

    chore: prepare example

--- packages/vue/examples/update-children/Array2Array.js
+++ packages/vue/examples/update-children/Array2Array.js
@@ -1,7 +1,26 @@
 import { h, ref } from '../dist/naux-vue.esm-bundler.js'
 
-const prevChildren = [h('p', {}, 'prevChildren item p 1'), h('span', {}, 'prevChildren item span 2')]
-const nextChildren = [h('p', {}, 'nextChildren item p 1'), h('p', {}, 'nextChildren item p 2')]
+// const prevChildren = [h('p', {}, 'prevChildren item p 1'), h('span', {}, 'prevChildren item span 2')]
+// const nextChildren = [h('p', {}, 'nextChildren item p 1'), h('p', {}, 'nextChildren item p 2')]
+
+// diff cases
+// case 1: starting on left
+// before: ( A B ) C
+// after: ( A B ) D E
+const prevChildren = [h('p', {}, 'A'), h('p', {}, 'B'), h('p', {}, 'C')]
+const nextChildren = [h('p', {}, 'A'), h('p', {}, 'B'), h('p', {}, 'D'), h('p', {}, 'E')]
+
+// case 2: starting on right
+// before: A ( B C )
+// after: D E ( B C )
+// const prevChildren = [h('p', {}, 'A'), h('p', {}, 'B'), h('p', {}, 'C')]
+// const nextChildren = [h('p', {}, 'D'), h('p', {}, 'E'), h('p', {}, 'B'), h('p', {}, 'C')]
+
+// case 3: prevChildren's number is more than nextChildren's
+// before: ( A B ) C
+// after: ( A B )
+// const prevChildren = [h('p', {}, 'A'), h('p', {}, 'B'), h('p', {}, 'C')]
+// const nextChildren = [h('p', {}, 'A'), h('p', {}, 'B')]
 
 export const Array2Array = {
   setup() {
