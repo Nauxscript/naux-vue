59bd6039bf204aee897dc7e4175627bca3a43a24
nauxscript <nauxscript@gmail.com>
2023-06-26 17:59:45 +0800

    chore: update example

--- packages/vue/examples/update-component/Child.js
+++ packages/vue/examples/update-component/Child.js
@@ -3,6 +3,6 @@ import { h } from '../dist/naux-vue.esm-bundler.js'
 export const Child = {
   name: 'Child',
   render() {
-    return h('div', {}, 'the value of count passing from parent is:')
+    return h('div', {}, `the value of count passing from parent is:${this.$props.msg}`)
   },
 }
