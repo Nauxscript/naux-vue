d651a4dd08535425fa831fcded58809742d9c399
nauxscript <nauxscript@gmail.com>
2023-06-27 10:17:43 +0800

    chore: update example

--- packages/vue/examples/nextTick/App.js
+++ packages/vue/examples/nextTick/App.js
@@ -1,12 +1,18 @@
-import { h, ref } from '../dist/naux-vue.esm-bundler.js'
+/* eslint-disable no-console */
+import { getCurrentInstance, h, nextTick, ref } from '../dist/naux-vue.esm-bundler.js'
 
 export const App = {
   name: 'App',
   setup() {
     const count = ref(0)
-    const handleCountIncrease100Times = () => {
+    const ctx = getCurrentInstance()
+    const handleCountIncrease100Times = async () => {
       for (let i = 0; i < 100; i++)
         count.value++
+
+      console.log(ctx)
+      await nextTick()
+      console.log(ctx)
     }
     return {
       handleCountIncrease100Times,
