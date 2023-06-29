c604b957e6244e4c340ec1df81be66f85c5d49e8
nauxscript <nauxscript@gmail.com>
2023-06-29 16:59:25 +0800

    feat: add complex template test case

--- packages/compiler-core/__tests__/parse.spec.ts
+++ packages/compiler-core/__tests__/parse.spec.ts
@@ -37,4 +37,27 @@ describe('parse', () => {
       })
     })
   })
+
+  test.only('complex template', () => {
+    const template = '<div>hello, {{message}}</div>'
+    const ast = baseParse(template)
+    const interpolation = ast.children[0]
+    expect(interpolation).toStrictEqual({
+      type: NodeTypes.ELEMENT,
+      tag: 'div',
+      children: [
+        {
+          type: NodeTypes.TEXT,
+          content: 'hello, ',
+        },
+        {
+          type: NodeTypes.INTERPOLATION,
+          content: {
+            type: NodeTypes.SIMPLE_EXPRESSION,
+            content: 'message',
+          },
+        },
+      ],
+    })
+  })
 })
