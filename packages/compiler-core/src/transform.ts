c1057ec2663cdc0ed9f1ea833d99d1dfdbf88b8f
nauxscript <nauxscript@gmail.com>
2023-07-01 22:41:11 +0800

    refactor: add createTransformContext

--- packages/compiler-core/src/transform.ts
+++ packages/compiler-core/src/transform.ts
@@ -1,15 +1,24 @@
 export function transform(root, options: Record<any, any> = {}) {
-  const { nodeTransforms } = options
-  if (nodeTransforms) {
-    for (let i = 0; i < nodeTransforms.length; i++)
-      nodeTransforms[i](root)
-  }
-  traverseNode(root, options)
+  const context = createTransformContext(root, options)
+  traverseNode(root, context)
 }
 
-function traverseNode(node, options) {
+function traverseNode(node, context) {
+  const { nodeTransforms } = context
+  if (nodeTransforms) {
+    for (let i = 0; i < nodeTransforms.length; i++)
+      nodeTransforms[i](node)
+  }
   if (!node.children)
     return
   for (let i = 0; i < node.children.length; i++)
-    transform(node.children[i], options)
+    traverseNode(node.children[i], context)
+}
+
+function createTransformContext(root: any, options: Record<any, any>) {
+  const context = {
+    root,
+    nodeTransforms: options.nodeTransforms,
+  }
+  return context
 }
