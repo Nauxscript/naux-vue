d55deb361643b396693ae7d63d54d66f5e0e5d5d
nauxscript <nauxscript@gmail.com>
2023-07-03 09:40:17 +0800

    test: update snapshot

--- packages/compiler-core/__tests__/codegen.spec.ts
+++ packages/compiler-core/__tests__/codegen.spec.ts
@@ -4,7 +4,7 @@ import { generate } from '../src/codegen'
 import { baseParse } from './../src/parse'
 describe('codegen', () => {
   it('basic string', () => {
-    const ast = baseParse('hello, nau')
+    const ast = baseParse('hello, naux')
     transform(ast)
     const { code } = generate(ast)
     expect(code).toMatchSnapshot()
