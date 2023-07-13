8c0e4cafa203bbc8308751c87b9332b98a4624bf
nauxscript mac-mini <nauxscript@gmail.com>
2023-07-13 22:11:43 +0800

    test: add watchEffect api happy path

---
+++ packages/runtime-core/__test__/apiWatch.spec.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from 'vitest'
+import { reactive } from '@naux-vue/reactivity'
+import { nextTick } from './../src/scheduler'
+describe('api: watch', () => {
+  it('effect', async () => {
+    const state = reactive({
+      count: 0,
+    })
+    let dummy
+    watchEffect(() => {
+      dummy = state.count
+    })
+    expect(dummy).toBe(0)
+
+    state.count++
+    expect(dummy).toBe(0)
+    await nextTick()
+    expect(dummy).toBe(1)
+  })
+})
